ad36be448c615c55337d49d924068b1bd9054588
Nilo <ndroock1@gmail.com>
2017-10-12 18:25:57 +0200

    @T8:R

--- src/main/java/org/nilostep/bota/dcp/data/domain/ConfigBC.java
+++ src/main/java/org/nilostep/bota/dcp/data/domain/ConfigBC.java
@@ -1,18 +1,32 @@
 package org.nilostep.bota.dcp.data.domain;
 
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Data
+@Entity
 public class ConfigBC {
 
+    @Id
     private String bcId;
 
-    private String bookmakerName;
-
-    private String competitionName;
-
-    private String bookmakerUrl;
+    private String bcUrl;
 
     private String cssSelector;
 
     private String rgxEventDescription;
 
     private String rgxMarketUrl;
+
+    @ManyToOne
+    @JoinColumn(name = "bookmaker_id")
+    private Bookmaker bookmaker;
+
+    @ManyToOne
+    @JoinColumn(name = "competition_id")
+    private Competition competition;
 }
