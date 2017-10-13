21dda4edd45b1fcf66bc47743be5b5f52d658c51
Nilo <ndroock1@gmail.com>
2017-10-13 11:47:02 +0200

    @T28:R

---
+++ src/main/java/org/nilostep/bota/dcp/data/repository/BookmakerRepository.java
@@ -0,0 +1,7 @@
+package org.nilostep.bota.dcp.data.repository;
+
+import org.nilostep.bota.dcp.data.domain.Bookmaker;
+import org.springframework.data.repository.CrudRepository;
+
+public interface BookmakerRepository extends CrudRepository<Bookmaker, String> {
+}
