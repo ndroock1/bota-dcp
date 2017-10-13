8d753f9b9e94297b34abd16ed56bd3a2ab7f8de0
Nilo <ndroock1@gmail.com>
2017-10-13 11:48:52 +0200

    @T29:R

---
+++ src/main/java/org/nilostep/bota/dcp/data/repository/ConfigBCRepository.java
@@ -0,0 +1,7 @@
+package org.nilostep.bota.dcp.data.repository;
+
+import org.nilostep.bota.dcp.data.domain.ConfigBC;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ConfigBCRepository extends CrudRepository<ConfigBC, Long> {
+}
