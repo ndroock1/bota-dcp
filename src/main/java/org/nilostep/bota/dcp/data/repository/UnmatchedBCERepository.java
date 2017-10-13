bafc20f88545a8c5e06ec34326efb80038706160
Nilo <ndroock1@gmail.com>
2017-10-13 11:52:44 +0200

    @T30:R

---
+++ src/main/java/org/nilostep/bota/dcp/data/repository/UnmatchedBCERepository.java
@@ -0,0 +1,7 @@
+package org.nilostep.bota.dcp.data.repository;
+
+import org.nilostep.bota.dcp.data.domain.UnmatchedBCE;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UnmatchedBCERepository extends CrudRepository<UnmatchedBCE, Long> {
+}
