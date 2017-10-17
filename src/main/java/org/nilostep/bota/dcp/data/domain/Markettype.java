93aaad825a1895fff5b2899a8bac2c98b2f081b1
Nilo <ndroock1@gmail.com>
2017-10-17 16:15:52 +0200

    @T42:R

--- src/main/java/org/nilostep/bota/dcp/data/domain/Markettype.java
+++ src/main/java/org/nilostep/bota/dcp/data/domain/Markettype.java
@@ -2,19 +2,19 @@ package org.nilostep.bota.dcp.data.domain;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 
 @Data
 @Entity
 public class Markettype {
 
     @Id
-    private String markettype;
+    @GeneratedValue
+    private Long mtId;
 
     @ManyToOne
     @JoinColumn(name = "eventtype")
     private Eventtype eventtype;
+
+    private String markettype;
 }
